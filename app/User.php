6d005334ead61681522723e26e740ad161bcd25e
pretendtrue <pretendtrue@qq.com>
2020-05-28 10:17:00 +0800

    用户假数据填充

--- app/User.php
+++ app/User.php
@@ -57,4 +57,15 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
 }
