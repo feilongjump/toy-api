270eaf5bc4b1acea26144e53512975d12fa77cb6
pretendtrue <pretendtrue@qq.com>
2020-05-29 17:23:06 +0800

    修改用户数据填充

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,6 +13,12 @@ class UserSeeder extends Seeder
     public function run()
     {
         $users = factory(User::class)->times(100)->make();
+
+        $user = $users->first();
+        $user->name = 'super';
+        $user->email = 'super@example.com';
+        $user->password = bcrypt('secret');
+
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
     }
 }
