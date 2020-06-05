cead8832e30cdc5c530313b0372d12de200978c3
pretendtrue <pretendtrue@qq.com>
2020-06-05 11:34:22 +0800

    时间格式序列化

--- app/Article.php
+++ app/Article.php
@@ -36,6 +36,17 @@ class Article extends Model
         });
     }
 
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
+
     public function content()
     {
         return $this->hasOne(Content::class);
--- app/Content.php
+++ app/Content.php
@@ -17,4 +17,15 @@ class Content extends Model
     protected $fillable = [
         'markdown',
     ];
+
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
 }
